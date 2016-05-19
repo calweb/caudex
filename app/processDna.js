fd4dc951c447d5cfc1bbc1410c1b8ec548d7c133
Calvin Webster <calweb@gmail.com>
2016-05-18 22:35:25 -0400

    consolidate all processing, including writing dna.json into its own module

--- app/processDna.js
+++ app/processDna.js
@@ -5,12 +5,14 @@ const { parse } = require('dna2json')
 const dnaParser = Rx.Observable.fromCallback(parse)
 const writeFile$ = Rx.Observable.fromNodeCallback(writeFile)
 const fileContents = Rx.Observable.fromNodeCallback(readFile)
+global.dnaFilePath = path.join(app.getPath('appData'), '/myDna.json')
 
 const processingDnaLog = () => console.log('Processing DNA now...')
 
 const parseDna = (filePath) => fileContents(filePath, 'utf8')
   .tap(processingDnaLog)
   .flatMap((file) => dnaParser(file))
+  .map((parsedFile) => writeFile$(dnaFilePath, JSON.stringify(parsedFile[1])))
   .catch((e) => Rx.Observable.throw(e))
 
 module.exports = parseDna
