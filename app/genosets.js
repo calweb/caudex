02e6ee66d87deb251a2dec1915ace764ab4588c8
Calvin Webster <calweb@gmail.com>
2016-09-16 20:08:45 -0400

    some cleanup

--- app/genosets.js
+++ app/genosets.js
@@ -1,100 +1,82 @@
-const { getGlobal } = require('electron').remote
 
-const gs144 = require('genoset-male')
-const gs101 = require('genoset-101') // lactose tolerant
-const gs119 = require('genoset-119') // 6.5x increased risk for breast cancer
-const gs122 = require('genoset-122') // increase risk of baldness
-const gs137 = require('genoset-137') // ~5.7x increased risk for thyroid cancer
-const gs150 = require('genoset-150') // "rapid metabolizer" phenotype for CYP2C19
-const gs152 = require('genoset-152') // "poor metabolizer" phenotype for CYP2C19
-const gs159 = require('genoset-159') // "rapid metabolizer" phenotype for CYP1A2
-const gs191 = require('genoset-191') // impaired NSAID metabolism
-const gs209 = require('genoset-209') // 1.7x increase risk panic disorder
-const gs211 = require('genoset-211') // problem metabolizing ethanol
-const gs216 = require('genoset-216') // increased risk of Alzheimer's and heart disease, but is protective against Hepatitis C-induced liver damage
-const gs228 = require('genoset-228') // sickle cell anemia
-const gs243 = require('genoset-243') // increased risk prostate cancer patients dying
-const gs248 = require('genoset-248') // lower risk parkinsons
-const gs291 = require('genoset-291') // lower heart attack risk
-const myDna = getGlobal('dnaFilePath')
 // there has to be a better way :|
 module.exports = {
     gs144: {
-      test: gs144,
+      test: require('genoset-male'),
       description: "Male"
     },
     gs101: {
-      test: gs101,
+      test: require('genoset-101'), // lactose tolerant
       description: "lactose tolerant"
 
     },
     gs119: {
-      test: gs119,
+      test: require('genoset-119'), // 6.5x increased risk for breast cancer
       description: "6.5x increased risk for breast cancer"
 
     },
     gs122: {
-      test: gs122,
+      test: require('genoset-122'), // increase risk of baldness
       description: "increase risk of baldness"
 
     },
     gs137: {
-      test: gs137,
+      test: require('genoset-137'), // ~5.7x increased risk for thyroid cancer
       description: "~5.7x increased risk for thyroid cancer"
 
     },
     gs150: {
-      test: gs150,
+      test: require('genoset-150'), // "rapid metabolizer" phenotype for CYP2C19
       description: "'rapid metabolizer' phenotype for CYP2C19"
 
     },
     gs152: {
-      test: gs152,
+      test: require('genoset-152'), // "poor metabolizer" phenotype for CYP2C19
       description: "'poor metabolizer' phenotype for CYP2C19"
 
     },
     gs159: {
-      test: gs159,
+      test: require('genoset-159'), // "rapid metabolizer" phenotype for CYP1A2
       description: "'rapid metabolizer' phenotype for CYP1A2"
 
     },
     gs191: {
-      test: gs191,
+      test: require('genoset-191'), // impaired NSAID metabolism
       description: "impaired NSAID metabolism"
 
     },
     gs209: {
-      test: gs209,
+      test: require('genoset-209'), // 1.7x increase risk panic disorder
       description: "1.7x increase risk panic disorder"
 
     },
     gs211: {
-      test: gs211,
+      test: require('genoset-211'), // problem metabolizing ethanol
       description: "problem metabolizing ethanol"
 
     },
     gs216: {
-      test: gs216,
+      test: require('genoset-216'), // increased risk of Alzheimer's and heart disease, but is protective against Hepatitis C-induced liver damage
       description: "increased risk of Alzheimer's and heart disease, but is protective against Hepatitis C-induced liver damage"
 
     },
     gs228: {
-      test: gs228,
+      test: require('genoset-228'), // sickle cell anemia
       description: "sickle cell anemia"
 
     },
     gs243: {
-      test: gs243,
+      test: require('genoset-243'), // increased risk prostate cancer patients dying
       description: "increased risk prostate cancer patients dying"
 
     },
     gs248: {
-      test: gs248,
+      test: require('genoset-248'), // lower risk parkinsons
       description: "lower risk parkinsons"
 
     },
     gs291: {
-      test: gs291,
+      test: require('genoset-291'), // lower heart attack risk
       description: "lower heart attack risk"
 
     }
